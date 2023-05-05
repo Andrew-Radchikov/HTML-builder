31a74b44f71d887c85a2646f7a470bd7b44913db
Andrew-Radchikov <superprobasite@yandex.ru>
2023-05-05 20:28:27 +0300

    fix: task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,11 +3,14 @@ let fs = require('fs');
 const path = require('path');
 const mainPath = path.join(__dirname, 'files');
 const copyPath = path.join(__dirname, 'files-copy');
-fs.mkdir(copyPath, { recursive: true }, err => {
-   if(err) throw err; 
-   
-});
 
+fs.stat(copyPath, function(err) {
+    if (!err) {
+              
+ listObjects(copyPath);
+ fs.mkdir(copyPath, { recursive: true }, err => {
+    if(err) throw err; 
+});
 fs.readdir(mainPath, {withFileTypes: true}, (err, files) => {
     files.forEach(file => {
         let wayMainFile = path.join(mainPath, file.name);
@@ -18,13 +21,41 @@ fs.readdir(mainPath, {withFileTypes: true}, (err, files) => {
                      if(err) throw err; 
                     
              });
-
-
     })
-
-
 })
 
+    }
+    else if (err.code === 'ENOENT') {
+        fs.mkdir(copyPath, { recursive: true }, err => {
+            if(err) throw err; 
+        });
+        fs.readdir(mainPath, {withFileTypes: true}, (err, files) => {
+            files.forEach(file => {
+                let wayMainFile = path.join(mainPath, file.name);
+                let wayCopyFile = path.join(copyPath, file.name);
+        
+        
+                fs.copyFile(wayMainFile, wayCopyFile, err => {
+                             if(err) throw err; 
+                            
+                     });
+            })
+        })
+        
+    }
+});
+
+
+
+function listObjects(direct){
+    let fs2 = require('fs');
+    fs2.readdir(direct, (err, files) => {
+       for (let file of files){
+        let dirTR = path.join(copyPath, file);
+        fs2.unlink(dirTR, err => {   });
+       }
+    });
+ }
 
 
 
@@ -32,41 +63,4 @@ fs.readdir(mainPath, {withFileTypes: true}, (err, files) => {
 
 
 
-// function copyDir(dir,name1, name2){
-//     let fs = require('fs');
-//     const path = require('path');
-//     const mainPath = path.join(__dirname, name1);
-//     const copyPath = path.join(__dirname, name2);
-//     fs.mkdir(copyPath, { recursive: true }, err => {
-//        if(err) throw err; 
-       
-//     });
-//     fs.readdir(mainPath, {withFileTypes: true}, (err, files) => {
-//         files.forEach(file => {
-//             let wayToFile = path.join(mainPath, file.name);
-//             fs.stat(wayToFile, (err, statistics) => {
-//                 if(!statistics.isDirectory()){
-//                     let wayMainFile = path.join(mainPath, file.name);
-//                     let wayCopyFile = path.join(copyPath, file.name);
-//                     fs.copyFile(wayMainFile, wayCopyFile, err => {
-//                         if(err) throw err; 
-//                     });
-//                 }
-//                 else{
-//                     console.log(file);
-//                     // copyDir(path.join(__dirname, name1),file.name,file.name);
-//                 }
-//             })
-           
-    
-    
-//         })
-    
-    
-//     })
-    
 
-// }
-
-
-// copyDir(__dirname,'files', 'files-copy' );
