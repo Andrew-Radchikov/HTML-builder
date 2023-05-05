164ed0c0c0f5766cee5486e4b8aa8f1567117b30
Andrew-Radchikov <superprobasite@yandex.ru>
2023-05-05 18:34:30 +0300

    fix: task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,3 +1,4 @@
+
 let fs = require('fs');
 const path = require('path');
 const mainPath = path.join(__dirname, 'files');
@@ -23,3 +24,49 @@ fs.readdir(mainPath, {withFileTypes: true}, (err, files) => {
 
 
 })
+
+
+
+
+
+
+
+
+// function copyDir(dir,name1, name2){
+//     let fs = require('fs');
+//     const path = require('path');
+//     const mainPath = path.join(__dirname, name1);
+//     const copyPath = path.join(__dirname, name2);
+//     fs.mkdir(copyPath, { recursive: true }, err => {
+//        if(err) throw err; 
+       
+//     });
+//     fs.readdir(mainPath, {withFileTypes: true}, (err, files) => {
+//         files.forEach(file => {
+//             let wayToFile = path.join(mainPath, file.name);
+//             fs.stat(wayToFile, (err, statistics) => {
+//                 if(!statistics.isDirectory()){
+//                     let wayMainFile = path.join(mainPath, file.name);
+//                     let wayCopyFile = path.join(copyPath, file.name);
+//                     fs.copyFile(wayMainFile, wayCopyFile, err => {
+//                         if(err) throw err; 
+//                     });
+//                 }
+//                 else{
+//                     console.log(file);
+//                     // copyDir(path.join(__dirname, name1),file.name,file.name);
+//                 }
+//             })
+           
+    
+    
+//         })
+    
+    
+//     })
+    
+
+// }
+
+
+// copyDir(__dirname,'files', 'files-copy' );
