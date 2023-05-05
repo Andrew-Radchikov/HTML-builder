60d1e955d8ce0a658c8776ecf03490494ff82f6a
Andrew-Radchikov <superprobasite@yandex.ru>
2023-05-05 18:13:56 +0300

    feat: task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
+let fs = require('fs');
+const path = require('path');
+const mainPath = path.join(__dirname, 'files');
+const copyPath = path.join(__dirname, 'files-copy');
+fs.mkdir(copyPath, { recursive: true }, err => {
+   if(err) throw err; 
+   
+});
 
+fs.readdir(mainPath, {withFileTypes: true}, (err, files) => {
+    files.forEach(file => {
+        let wayMainFile = path.join(mainPath, file.name);
+        let wayCopyFile = path.join(copyPath, file.name);
+
+
+        fs.copyFile(wayMainFile, wayCopyFile, err => {
+                     if(err) throw err; 
+                    
+             });
+
+
+    })
+
+
+})
